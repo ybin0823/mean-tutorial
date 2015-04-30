041656569278c7cfc387008d969d1ac01cabc1ba
Youngbin Jang <jyb0823@gmail.com>
2015-04-30 20:35:29 +0900

    http GET 으로 요청하여 dummy data받고 출력하기

--- server.js
+++ server.js
@@ -3,6 +3,31 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/contactList', function(req, res) {
+	console.log("I received from GET a request")
+
+	person1 = {
+		name : 'Tim', 
+		email : 'tim@gmail.com',
+		number : '(111) 111-1111'
+	};
+
+	person2 = {
+		name : 'Emily', 
+		email : 'emily@gmail.com',
+		number : '(222) 222-2222'
+	};
+
+	person3 = {
+		name : 'John', 
+		email : 'john@gmail.com',
+		number : '(333) 333-3333'
+	};
+
+	var contactList = [person1, person2, person3];
+	res.json(contactList);
+});
+
 app.listen(3000, function() {
 	console.log("Server started at port 3000");
 });
