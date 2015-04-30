d860d0fe3e4736aa9bcc88b2d77f9eb32001c1e3
Youngbin Jang <jyb0823@gmail.com>
2015-04-30 22:15:17 +0900

    데이터 추가시 자동으로 화면 refresh를 위한 window.location.reload(true) 추가

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,15 +2,19 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 	console.log("Hello World from controller");
 
+
 	$http.get('/contactList').success(function(res) {
 		console.log("I got the data i requested");
 		$scope.contactList = res;
-	})
+		$scope.contact = "";
+	});
+
 
 	$scope.addContact = function() {
 		console.log($scope.contact);
 		$http.post('/contactList', $scope.contact).success(function(res) {
 			console.log(res);
+			window.location.reload(true);
 		});
 	};
 }]);
