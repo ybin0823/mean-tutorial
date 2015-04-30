733a5e430cc2ab7009b9b0832d40a26dc9668429
Youngbin Jang <jyb0823@gmail.com>
2015-04-30 21:42:24 +0900

    mongodb 설치하고 mongojs 모듈 추가하여 dummy data를 db에 저장 후 db데이터를 불러와서 결과 출력

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 tutorial from http://www.youtube.com/playlist?list=PLX2HoWE32I8Nkzw2TqcifObuhgJZz8a0U
 
 ### Need To
-1. Install node.js
+1. Install Node.js
 http://nodejs.org
 
+2. Install MongoDB
+http://www.mongodb.org
+
+>use contactList
+>db.contactList.insert(data)
--- server.js
+++ server.js
@@ -1,31 +1,17 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactList', ['contactList']);
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/contactList', function(req, res) {
 	console.log("I received from GET a request")
 
-	person1 = {
-		name : 'Tim', 
-		email : 'tim@gmail.com',
-		number : '(111) 111-1111'
-	};
-
-	person2 = {
-		name : 'Emily', 
-		email : 'emily@gmail.com',
-		number : '(222) 222-2222'
-	};
-
-	person3 = {
-		name : 'John', 
-		email : 'john@gmail.com',
-		number : '(333) 333-3333'
-	};
-
-	var contactList = [person1, person2, person3];
-	res.json(contactList);
+	db.contactList.find(function (err, docs) {
+		console.log(docs);
+		res.json(docs);
+	});
 });
 
 app.listen(3000, function() {
