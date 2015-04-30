6a94eda137dcc82f660d76b849b9c2cfd39bf329
Youngbin Jang <jyb0823@gmail.com>
2015-04-30 20:20:39 +0900

    index.html로 응답 보내기

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-	res.send("Hello, World");
-})
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function() {
 	console.log("Server started at port 3000");
