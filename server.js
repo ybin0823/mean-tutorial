5be01df5dfb5b5a875b1a7bb8e0a6a0a75dc9154
Youngbin Jang <jyb0823@gmail.com>
2015-05-05 22:01:49 +0900

    mongojs -> mongoose로 변환

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
-var mongojs = require('mongojs');
-var db = mongojs('contactList', ['contactList']);
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/contactList');
+
+var contactListSchema = mongoose.Schema({
+	name: String,
+	email: String,
+	number: String
+}, { collection: 'contactList' });
+
+var ContactList = mongoose.model('contactList', contactListSchema);
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/resources'));
@@ -10,42 +19,44 @@ app.use(bodyParser.json());
 
 app.get('/contactList', function (req, res) {
 	console.log("I received from GET a request")
-
-	db.contactList.find(function (err, docs) {
+	ContactList.find(function (err, docs) {
 		console.log(docs);
 		res.json(docs);
 	});
 });
 
 app.post('/contactList', function (req, res) {
-	console.log(req.body);
-	db.contactList.insert(req.body, function (err, doc) {
+	console.log("I received from POST a request : ", req.body);
+
+	// 새 인스턴스를 생성해서 저장하는 방법(save)
+	// var newContactList = new ContactList({
+	// 	name: req.body.name,
+	// 	email: req.body.email,
+	// 	number: req.body.number
+	// });
+
+	// newContactList.save(function (err, doc) {
+	// 	res.json(doc);
+	// });
+	ContactList.create(req.body, function (err, doc) {
 		res.json(doc);
 	});
 });
 
 app.delete('/contactList/:id', function (req, res) {
-	var id = req.params.id;
-	console.log(id);
-	db.contactList.remove({ _id: mongojs.ObjectId(id) }, function (err, doc) {
+	console.log("I received from DELETE a request : ", req.params.id);
+	ContactList.findByIdAndRemove(req.params.id, function (err, doc) {
 		res.json(doc);
 	});
 });
 
 app.put('/contactList/:id', function (req, res) {
-	var id = req.params.id;
-	console.log(req.body.name);
-	db.contactList.findAndModify(
-	{ query: { _id: mongojs.ObjectId(id) }, 
-		update: {$set: { 
-			name: req.body.name, 
-			email: req.body.email, 
-			number: req.body.number 
-		}
-	},
-		new: true
-	}, function (err, doc) {
-		console.log(doc);
+	console.log("I received from PUT a request : ", req.params.id);
+	ContactList.findByIdAndUpdate(req.params.id, { 
+		name: req.body.name, 
+		email: req.body.email, 
+		number: req.body.number 
+	}, function (err, doc) { 
 		res.json(doc);
 	});
 });
